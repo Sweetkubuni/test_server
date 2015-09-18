35f2d501e75098dfdccd86097455e6e68739b384
Sweetkubuni <justinsweng@gmail.com>
2015-09-17 21:23:46 -0400

    Update control.hpp

--- control.hpp
+++ control.hpp
@@ -1,12 +1,13 @@
 #ifndef CONTROL_HPP
 #define CONTROL_HPP
 #include "threadsafe_queue.hpp"
+#include  <memory>
 class control
 {
     protected:
 	threadsafe::queue< std::string > out_mq;
 	threadsafe::queue< std::string > in_mq;
-	unique_ptr<std::thread> th;
+	std::unique_ptr<std::thread> th;
 	bool keep_alive;
 	void run();
 	public:
