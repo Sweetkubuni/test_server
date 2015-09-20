033804cf0f473b793092eb2f47db4a7bf053f071
Sweetkubuni <justinsweng@gmail.com>
2015-09-20 16:06:43 -0400

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -105,7 +105,7 @@ void server::run()
         if(req)
         {
             /* kills the server */
-            if(req->compare("KILL") == 0)
+            if(req->find("KILL") != std::string::npos)
             {
                 keep_alive = 0;
             }
